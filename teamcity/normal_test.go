902709362472b6a67f3a2503f2dd7ba5e7514fca
Daniel Lipovetsky <dlipovetsky@d2iq.com>
2021-02-08 16:48:16 -0800

    teamcity: Change log message prefixes to use test names

--- teamcity/normal_test.go
+++ teamcity/normal_test.go
@@ -15,19 +15,19 @@ const (
 var _ = Describe("When running", func() {
 
 	It("should write to os.Stdout", func() {
-		util.LogFor(os.Stdout, duration, "test A")
+		util.LogFor(os.Stdout, 3*duration, "should write to os.Stdout")
 	})
 
 	It("should write to os.Stdout, and finally fail", func() {
-		util.LogFor(os.Stdout, 2*duration, "test C")
+		util.LogFor(os.Stdout, 2*duration, "should write to os.Stdout, and finally fail")
 		Fail("this test is supposed to fail")
 	})
 
 	It("should write to os.Stderr", func() {
-		util.LogFor(os.Stdout, 3*duration, "test B")
+		util.LogFor(os.Stdout, 3*duration, "should write to os.Stderr")
 	})
 
 	It("should write to GinkgoWriter", func() {
-		util.LogFor(GinkgoWriter, 3*duration, "test C")
+		util.LogFor(GinkgoWriter, 3*duration, "should write to GinkgoWriter")
 	})
 })
