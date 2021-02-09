193c676dd2ee9b801b47df02eab8c05a1a4848ec
Daniel Lipovetsky <dlipovetsky@d2iq.com>
2021-02-08 22:14:04 -0800

    teamcity: output default reporter config

--- teamcity/suite_test.go
+++ teamcity/suite_test.go
@@ -17,20 +17,19 @@ func TestExample(t *testing.T) {
 	RegisterFailHandler(Fail)
 
 	rNames := os.Getenv("GINKGO_REPORTERS")
-	fmt.Fprintf(GinkgoWriter, "GINKGO_REPORTERS=%s\n", rNames)
 
 	var rs []Reporter
 	if strings.Contains(rNames, "teamcity") {
 		t.Log("Using TeamCity reporter")
-		rs = append(rs, reporters.NewTeamCityReporter(os.Stdout))
+		tcr := reporters.NewTeamCityReporter(os.Stdout)
+		rs = append(rs, tcr)
 	}
 	if strings.Contains(rNames, "junit") {
 		t.Log("Using JUnit reporter")
 		filename := fmt.Sprintf("junit-node-%d.xml", config.GinkgoConfig.ParallelNode)
-		rs = append(rs, reporters.NewJUnitReporter(filename))
+		jur := reporters.NewJUnitReporter(filename)
+		rs = append(rs, jur)
 	}
-
-	fmt.Fprintf(GinkgoWriter, "GinkgoParallelNode: %d\n", GinkgoParallelNode())
 
 	if rNames == "" || strings.Contains(rNames, "default") {
 		RunSpecsWithDefaultAndCustomReporters(t, "TeamCity Reporting Experiment", rs)
@@ -39,8 +38,12 @@ func TestExample(t *testing.T) {
 	}
 }
 
-var _ = SynchronizedAfterSuite(func() {
-	// no-op
-}, func() {
-	// no-op
-})
+var _ = SynchronizedAfterSuite(
+	func() {
+		fmt.Fprintf(os.Stderr, "GINKGO_REPORTERS=%s\n", os.Getenv("GINKGO_REPORTERS"))
+		fmt.Fprintf(os.Stderr, "GinkgoParallelNode#: %d\n", GinkgoParallelNode())
+		fmt.Fprintf(os.Stderr, "DefaultReporterConfig: %+v\n", config.DefaultReporterConfig)
+	},
+	func() {
+		// no-op
+	})
