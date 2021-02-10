c03c8bdea449d18d918a27f170c740ee8b0abf60
Daniel Lipovetsky <dlipovetsky@d2iq.com>
2021-02-09 17:31:36 -0800

    teamcity: Add test that writes to stdout and stderr concurrently

--- teamcity/normal_test.go
+++ teamcity/normal_test.go
@@ -2,6 +2,7 @@ package a_test
 
 import (
 	"os"
+	"sync"
 	"time"
 
 	"daniel.lipovetsky.me/ginkgo-experiments/util"
@@ -18,13 +19,24 @@ var _ = Describe("When running", func() {
 		util.LogFor(os.Stdout, 3*duration, "should write to os.Stdout")
 	})
 
-	It("should write to os.Stdout, and finally fail", func() {
-		util.LogFor(os.Stdout, 2*duration, "should write to os.Stdout, and finally fail")
-		Fail("this test is supposed to fail")
+	It("should write to os.Stderr", func() {
+		util.LogFor(os.Stderr, 2*duration, "should write to os.Stderr")
 	})
 
-	It("should write to os.Stderr", func() {
-		util.LogFor(os.Stdout, 3*duration, "should write to os.Stderr")
+	It("should write to os.Stdout, os.Stderr, and finally fail", func() {
+		wg := sync.WaitGroup{}
+		wg.Add(1)
+		go func() {
+			util.LogFor(os.Stdout, 3*duration, "should write to os.Stdout")
+			wg.Done()
+		}()
+		wg.Add(1)
+		go func() {
+			go util.LogFor(os.Stderr, 3*duration, "should write to os.Stderr")
+			wg.Done()
+		}()
+		wg.Wait()
+		Fail("this test is supposed to fail")
 	})
 
 	It("should write to GinkgoWriter", func() {
